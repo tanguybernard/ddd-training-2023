4aab6a62144723c7ccd6ca21612fd98b28fbadc6
tanguybernard <bernardtanguy1pro@gmail.com>
2023-06-07 21:05:52 +0200

    setup

---
+++ ddd-2023-06-07-vip-emergency/src/main/java/com/zenika/training/TrainingApplication.java
@@ -0,0 +1,23 @@
+package com.zenika.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class TrainingApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context =
+                SpringApplication.run(TrainingApplication.class, args);
+
+
+
+    }
+
+//test 
+
+// Hello !
+
+
+}
